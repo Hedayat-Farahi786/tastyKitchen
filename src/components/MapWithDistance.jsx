d732a0b0c4415fc4e62a2611db987c7712b44d1a
Hedayat Farahi <hedayatfarahi8@gmail.com>
2023-09-22 22:15:27 +0200

    update the marker for map

--- src/components/MapWithDistance.jsx
+++ src/components/MapWithDistance.jsx
@@ -9,6 +9,9 @@ import {
 import L from "leaflet";
 import { getDistance } from "geolib";
 
+// Import your custom marker icon image
+import customMarkerIcon from "../assets/mark.png";
+
 const MapWithDistance = ({ origin, destination }) => {
   const [distance, setDistance] = useState(null);
   const [route, setRoute] = useState([]);
@@ -23,33 +26,43 @@ const MapWithDistance = ({ origin, destination }) => {
     setRoute(routeCoordinates);
   }, [origin, destination]);
 
+  // Create a custom Icon object for the markers
+  const customIcon = L.icon({
+    iconUrl: customMarkerIcon, // Path to your custom marker icon image
+    iconSize: [32, 32], // Adjust the size as needed
+    iconAnchor: [16, 32], // Adjust the anchor point if necessary
+  });
+
   return (
     <div className="w-full m-0">
-      {/* {distance && <p>Distance: {distance} km</p>} */}
       <MapContainer
-        center={[origin.lat, origin.lng]}
-        zoom={14}
-        style={{ width: "100%", height: "400px" }}
+        center={[
+          (origin.lat + destination.lat) / 2,
+          (origin.lng + destination.lng) / 2,
+        ]}
+        zoom={13}
+        style={{ width: "100%", height: "300px" }}
+        className="z-40"
+        zoomControl={false}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
 
-        {/* Marker for the restaurant */}
-        <Marker position={[destination.lat, destination.lng]}>
-          <Popup>Restaurant</Popup>
+        {/* Marker for the restaurant with custom icon */}
+        <Marker position={[destination.lat, destination.lng]} icon={customIcon}>
+          <Popup>Tasty Kitchen</Popup>
         </Marker>
 
-        {/* Marker for the user's location */}
-        <Marker position={[origin.lat, origin.lng]}>
-          <Popup>User Location</Popup>
+        {/* Marker for the user's location with custom icon */}
+        <Marker position={[origin.lat, origin.lng]} icon={customIcon}>
+          <Popup>Deine Adresse</Popup>
         </Marker>
 
         {/* Polyline representing the route */}
         <Polyline
           positions={route}
-          color="blue"
+          color="#e53935"
           pathOptions={{ weight: 4, dashArray: "10 10" }}
         />
       </MapContainer>
