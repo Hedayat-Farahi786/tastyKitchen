a90c907b781ea2d961eb213c152663579d1a059b
Hedayat Farahi <hedayatfarahi8@gmail.com>
2024-06-13 22:57:04 +0200

    add web socket

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -16,7 +16,11 @@ import toast from "react-hot-toast";
 
 import io from "socket.io-client";
 const socket = io("https://tasty-kitchen-socket.vercel.app", {
-  transports: ["websocket"], // Explicitly use WebSocket transport
+  transports: ["websocket", "polling"], // Explicitly use WebSocket transport
+  path: "/socket",
+  reconnectionAttempts: 5,
+  timeout: 20000,
+  reconnection: true,
   withCredentials: true
 });
 
